5ae6dab7647c98be992fe023f14b587d33501552
Ben <quillaja@hotmail.com>
2018-02-09 05:15:06 -0800

    First working example. Actually works rather well!!

--- attractor/main.go
+++ attractor/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"time"
+
+	"github.com/quillaja/goutil/rand"
+
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/pixelgl"
 	"golang.org/x/image/colornames"
@@ -17,9 +21,59 @@ func run() {
 		panic(err)
 	}
 
-	win.Clear(colornames.Skyblue)
+	// 10 particles randomly positioned on screen
+	// with random starting velocities
+	balls := []*Particle{}
+	for i := 0; i < 10; i++ {
+		ball := NewParticleDefault()
+		ball.Pos.X, ball.Pos.Y = rand.Float64NM(10, 1000), rand.Float64NM(10, 700) // top center of screen
+		ball.Vel.X = rand.Float64NM(-20, 20)
+		ball.Vel.Y = rand.Float64NM(-20, 20)
+		ball.Radius = 2
+		ball.Mass = 1
+		ball.Color = colornames.Blue
+		balls = append(balls, ball)
+	}
+
+	// mass is kinda "high"
+	attractor := NewParticleParams(512, 384, 10000, 5, colornames.Red)
+
+	// gravity acts down
+	gravity := pixel.V(0, -10)
+
+	start := time.Now()
 
 	for !win.Closed() {
+		dt := time.Since(start)
+		start = time.Now()
+
+		showVecs := win.Pressed(pixelgl.KeyV)
+
+		// update state
+		for _, ball := range balls {
+			ball.ResetForce() // have to do when using gravitation
+			if win.Pressed(pixelgl.KeySpace) {
+				// have to reapply gravity each time
+				ball.ApplyStatic(gravity)
+			}
+			ball.ApplyGravitation(attractor.Body) // gravity between ball and attractor
+			ball.UpdatePosition(dt.Seconds())
+			// fmt.Printf("ball: P: %s\tF: %s\n", ball.Pos, ball.Force)
+		}
+
+		// clear window
+		win.Clear(colornames.Whitesmoke)
+
+		// draw balls
+		for _, ball := range balls {
+			ball.Draw(showVecs)
+			ball.GetVisual().Draw(win)
+		}
+
+		// draw attractor
+		attractor.Draw(false) // won't move anyway
+		attractor.GetVisual().Draw(win)
+
 		win.Update()
 	}
 }
---
+++ attractor/particle.go
@@ -0,0 +1,165 @@
+package main
+
+import (
+	"image/color"
+
+	"golang.org/x/image/colornames"
+
+	"github.com/faiface/pixel/imdraw"
+
+	px "github.com/faiface/pixel"
+)
+
+// G is the universal gravitational constant
+const G = 100.0
+
+// Gravitation calculates the force exerted on "a" by "b".
+func Gravitation(a, b Body) px.Vec {
+	// F = G * (m1*m2)/|d2-d1|^2 * rhat
+	atob := a.Pos.To(b.Pos) // vector from a to b
+	d := atob.Len()         // d should always be positive
+	dsq := d * d
+	if dsq <= 0 { // prevent divide by zero
+		dsq = 1e-1
+	}
+	f := atob.Unit().Scaled((G * a.Mass * b.Mass) / dsq)
+	if f.Len() > 100 {
+		f = f.Unit().Scaled(100)
+	}
+	return f
+}
+
+// Drawable provides methods to allow objects to be drawn.
+type Drawable interface {
+	Draw(bool)
+	GetVisual() *imdraw.IMDraw
+}
+
+// Body is the "physics" part of an object.
+type Body struct {
+	Pos   px.Vec
+	Vel   px.Vec
+	Force px.Vec
+	Mass  float64
+}
+
+// ResetForce does exactly what you think it does.
+func (b *Body) ResetForce() {
+	b.Force.X, b.Force.Y = 0, 0
+}
+
+// ApplyGravitation calculates the gravitational force between this body and
+// the others, accumulating the total force applied in Body.Force.
+func (b *Body) ApplyGravitation(others ...Body) {
+	// accumulate forces acting on this body
+	// ... could use ApplyStatic() here, but I'd have to pack/unpack all the
+	// force vectors. This is simple enough to just duplicate the calculations.
+	for _, o := range others {
+		f := Gravitation(*b, o)
+		b.Force.X += f.X
+		b.Force.Y += f.Y
+	}
+}
+
+// ApplyStatic applies the static force(s) to Body.Force.
+func (b *Body) ApplyStatic(forces ...px.Vec) {
+	for _, f := range forces {
+		b.Force.X += f.X
+		b.Force.Y += f.Y
+	}
+}
+
+// ApplyResistance applies force(s) to the body in the direction *opposite*
+// its velocity and with the given magnitudes.
+// TODO: check for "real" way of calculating this.
+func (b *Body) ApplyResistance(magnitudes ...float64) {
+	// As in ApplyGravitation(), just redo calcs.
+	for _, m := range magnitudes {
+		f := b.Vel.Unit().Scaled(-m)
+		b.Force.X += f.X
+		b.Force.Y += f.Y
+	}
+}
+
+// UpdatePosition tells the object to recalculate its position based on the
+// accumlated total force (Body.Force) applied to it in the duration
+// of "dt" seconds. Assumes all forces have already been applied.
+func (b *Body) UpdatePosition(dt float64) {
+	// calculate new acceleration
+	// F=ma, a = F/m
+	acc := b.Force.Scaled(1 / b.Mass)
+	// calculate new velocity
+	// dv = a*dt
+	b.Vel.X += acc.X * dt
+	b.Vel.Y += acc.Y * dt
+	// calculate new position
+	// dp = p + dv
+	b.Pos.X += b.Vel.X * dt
+	b.Pos.Y += b.Vel.Y * dt
+}
+
+// Particle is the overall object in the world, consisting of a "body"
+// for physics-like movement and a graphical representation.
+type Particle struct {
+	Body
+
+	Radius float64
+	Color  color.Color
+	visual *imdraw.IMDraw
+	dirty  bool // does it need redrawn
+}
+
+// NewParticleDefault makes a "default" Particle.
+// Has mass and diameter of 1, white color. Other fields are zero.
+func NewParticleDefault() *Particle {
+	return &Particle{
+		Body:   Body{Mass: 1},
+		Radius: 1,
+		Color:  colornames.White,
+		visual: imdraw.New(nil),
+		dirty:  true}
+}
+
+// NewParticleParams makes a new Particle with the parameters YOU can decide!
+func NewParticleParams(x, y, mass, diam float64, color color.Color) *Particle {
+	p := NewParticleDefault()
+	p.Pos.X = x
+	p.Pos.Y = y
+	p.Mass = mass
+	p.Radius = diam
+	p.Color = color
+	return p
+}
+
+// Draw tells the particle to redraw itself.
+func (p Particle) Draw(showVectors bool) {
+	if p.dirty {
+		p.visual.Reset()
+		p.visual.Clear()
+
+		// draw particle
+		p.visual.Color = p.Color
+		p.visual.Push(p.Pos)
+		p.visual.Circle(p.Radius, 0)
+
+		if showVectors {
+			// draw velocity vector
+			p.visual.Color = colornames.Green
+			p.visual.Push(p.Pos, p.Pos.Add(p.Vel))
+			p.visual.Line(1)
+
+			// draw force vector
+			p.visual.Color = colornames.Black
+			p.visual.Push(p.Pos, p.Pos.Add(p.Force))
+			p.visual.Line(2)
+		}
+
+		// p.dirty = false
+	}
+
+}
+
+// GetVisual returns the particle's visual representation.
+func (p Particle) GetVisual() *imdraw.IMDraw {
+	return p.visual
+}
