61a45653851b526deb847fbf137950e7cdcc1adc
Ben <quillaja@hotmail.com>
2018-02-12 03:27:38 -0800

    added a custom implementation of pixel.PictureData to use to hold the Particle trails in a relatively efficient manner. Drawing the trails to the window could be improved, though.

--- attractor/main.go
+++ attractor/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	grand "math/rand"
+	"os"
 	"time"
 
 	"github.com/quillaja/goutil/rand"
@@ -14,9 +15,10 @@ import (
 )
 
 const (
-	width  = 1200
-	height = 800
-	title  = "Particles"
+	width      = 1200
+	height     = 800
+	title      = "Particles"
+	trailAlpha = 0.2
 )
 
 func run() {
@@ -25,6 +27,15 @@ func run() {
 	var numAttractors int
 	flag.IntVar(&numBalls, "n", 10, "Number of balls.")
 	flag.IntVar(&numAttractors, "a", 1, "Number of attractors.")
+	flag.Usage = func() {
+		msg :=
+			`Shows a simple particle physics simulation. Hold SPACE to view the object's 
+velocity (thin green line) and force (thick black line) vectors. 
+Hold T to view the paths of all objects. Hold R to apply a resistance and G to 
+apply gravity in the downward direction. (requires OpenGL 3.3+)`
+		fmt.Fprintln(os.Stderr, msg)
+		flag.PrintDefaults()
+	}
 	flag.Parse()
 
 	grand.Seed(time.Now().UnixNano())
@@ -48,7 +59,7 @@ func run() {
 		ball.Vel.Y = rand.Float64NM(-20, 20)
 		ball.Radius = 2
 		ball.Mass = 1
-		ball.Color = colornames.Blue
+		ball.Color = colornames.Royalblue
 		balls = append(balls, ball)
 	}
 
@@ -56,14 +67,23 @@ func run() {
 	attractors := []*Particle{}
 	for i := 0; i < numAttractors; i++ {
 		// mass is kinda "high"?
-		a := NewParticleParams(rand.Float64NM(0, width), rand.Float64NM(0, height), 1000, 6, colornames.Red)
-		// a.RepulsorDistance = 20
+		a := NewParticleParams(
+			rand.Float64NM(0, width),
+			rand.Float64NM(0, height),
+			rand.Float64NM(800, 2000),
+			rand.Float64NM(4, 10),
+			colornames.Orangered)
+		// a.RepulsorDistance = 10
 		attractors = append(attractors, a)
 	}
 
 	// gravity acts down
 	gravity := pixel.V(0, -10)
 	resistance := 5.0
+
+	trails := new(AlphaTrail)
+	// bgTrails := trails.GetSprite()
+	trailsMatrix := pixel.IM.Moved(pixel.V(width/2, height/2))
 
 	frames := 0
 	timer := time.NewTicker(time.Second)
@@ -72,10 +92,19 @@ func run() {
 		dt := time.Since(start)
 		start = time.Now()
 
-		showVecs := win.Pressed(pixelgl.KeyV)
+		// drawing ///////////////////////////////////////////////////
 
 		// clear window
 		win.Clear(colornames.White)
+
+		// draw trails as background
+		if win.Pressed(pixelgl.KeyT) {
+			// bgTrails.Set(trails, trails.Bounds())
+			// bgTrails.Draw(win, trailsMatrix)
+			trails.GetSprite().DrawColorMask(win, trailsMatrix, colornames.Black)
+		}
+
+		showVecs := win.Pressed(pixelgl.KeySpace)
 
 		// draw balls
 		for _, ball := range balls {
@@ -89,12 +118,14 @@ func run() {
 			attractor.GetVisual().Draw(win)
 		}
 
+		// finish window drawing
 		win.Update()
 
-		// update state
+		// update state //////////////////////////////////////////////////
+
 		for _, ball := range balls {
 			ball.ResetForce()
-			if win.Pressed(pixelgl.KeySpace) {
+			if win.Pressed(pixelgl.KeyG) {
 				ball.ApplyForce(gravity)
 			}
 			if win.Pressed(pixelgl.KeyR) {
@@ -104,8 +135,13 @@ func run() {
 				ball.ApplyForce(Gravitation(&ball.Body, &a.Body)) // gravity between ball and attractor
 			}
 			ball.UpdatePosition(dt.Seconds())
+
+			if trails.Bounds().Contains(ball.Pos) {
+				trails[int(ball.Pos.X)][int(ball.Pos.Y)] += trailAlpha
+			}
 		}
 
+		// update framerate in window title
 		select {
 		case <-timer.C:
 			win.SetTitle(fmt.Sprintf("%s - %d fps", title, frames))
@@ -113,7 +149,6 @@ func run() {
 		default:
 			frames++
 		}
-
 	}
 
 	timer.Stop()
@@ -121,4 +156,28 @@ func run() {
 
 func main() {
 	pixelgl.Run(run)
+}
+
+// AlphaTrail is a simple data structure to allow the object paths
+// to be tracked in a quick and (relatively) memory efficient manner.
+// Each object increments the value at AlphaTrail[x][y] by some fractional
+// amount. The value at any particular (x,y) is an alpha value used
+// when drawing the image to screen.
+type AlphaTrail [width][height]float64
+
+// Bounds provides the bounding rectangle, for implementing pixel.Picture
+func (a *AlphaTrail) Bounds() pixel.Rect {
+	return pixel.R(0, 0, width, height)
+}
+
+// Color provides a color value at a particular location. For implementing
+// pixel.PictureColor.
+func (a *AlphaTrail) Color(at pixel.Vec) pixel.RGBA {
+	x, y := int(at.X), int(at.Y)
+	return pixel.RGBA{R: a[x][y], G: a[x][y], B: a[x][y], A: a[x][y]}
+}
+
+// GetSprite is a convenient way to get a pixel.Sprite of this data, for drawing.
+func (a *AlphaTrail) GetSprite() *pixel.Sprite {
+	return pixel.NewSprite(a, a.Bounds())
 }
